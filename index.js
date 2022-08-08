21311da467ec7a2b4d227605bd11d66f877ba1b9
Saeed Borji <saeedborji5@gmail.com>
2022-08-08 17:05:14 +0200

    08/08/2022 _ commit 2

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ function show() {
   sec++;
   const el = <p>{hour}:{min}:{sec}</p>;
   ReactDOM.render(el, document.getElementById('root'));
+  if(sec => 60)
+  {
+    min++;
+    sec = 0;
+  }
+  if(min => 60)
+  {
+    hour++;
+    min = 0;
+    sec = 0;
+  }
 }
 
 setInterval(show, 1000);
