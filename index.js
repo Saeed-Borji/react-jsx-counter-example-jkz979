1424083bdad1ee07b87acbf8c35fd7bb93512842
Saeed Borji <saeedborji5@gmail.com>
2022-08-08 17:01:01 +0200

    Update index.js
    create 08/08/2022

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './style.css';
 
 let sec = 0;
+let min = 0;
+let hour = 0;
 
 function show() {
   sec++;
-  const el = <p>{sec}</p>;
+  const el = <p>{hour}:{min}:{sec}</p>;
   ReactDOM.render(el, document.getElementById('root'));
 }
 
